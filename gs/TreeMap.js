d8311d3fc272f2b404344bdeca30e2d68dcc1324
owen550 <omoses611@gmail.com>
2025-11-19 11:59:33 +0700

    Inisiasi Tree Untuk Map

---
+++ gs/TreeMap.js
@@ -0,0 +1,10 @@
+export const TreeMap = {
+    paren: null,
+    isConquereByRed: 0,
+    isConquereByBlue: 0,
+    onTree: null,
+    childe: [null,null,null,null,null,null],
+    fungsiTes: (param) => {
+        console.log('Isi : ' + param);
+    }
+}
